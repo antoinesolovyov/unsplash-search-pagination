fc40a69f6b2ff4ec09cbd29d6dc15115ada5f05e
antoinesolovyov <antoinesolovyov@gmail.com>
2020-01-28 15:44:31 +0300

    Final commit

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,15 +5,26 @@ import "./Main.css";
 export default function Main(props) {
   const getPhotos = () => {
     return props.photos.map(photo => (
-      <img key={photo.id} id={photo.id} src={photo.urls.small} />
+      <img
+        key={photo.id}
+        id={photo.id}
+        src={photo.urls.small}
+        name={photo.urls.raw}
+      />
     ));
   };
 
   const photoOnClick = event => {
     if (!event.target.className) {
       event.target.className = "active";
+      const tmp = event.target.src;
+      event.target.src = event.target.name;
+      event.target.name = tmp;
     } else {
       event.target.className = "";
+      const tmp = event.target.src;
+      event.target.src = event.target.name;
+      event.target.name = tmp;
     }
   };
 
